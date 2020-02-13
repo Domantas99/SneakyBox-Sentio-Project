da6f6fc1e29187ced210d2c7fc6c610e38f193ad
domantas99 <domantas.greicius@gmail.com>
2020-02-13 18:14:58 +0200

    Started implementing user backend

---
+++ back-end sentio/Sentio/Sentio/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sentio.DTO;
+using Sentio.RequestResults;
+
+namespace Sentio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<UserValidationResult>> ValidateUser([FromBody] UserRegistrationForm user) {
+
+            return null;
+        }
+
+    }
+}
---
+++ back-end sentio/Sentio/Sentio/RequestResults/UserValidationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sentio.RequestResults
+{
+    public class UserValidationResult
+    {
+        public bool IsValid { get; set; } 
+        public string Message { get; set; }
+        public Guid Id { get; set; }
+    }
+}
